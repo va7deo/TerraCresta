e68a9737ec5912b8002172184ad4b85baab4dd88
Darren <va7deo@gmail.com>
2022-04-15 22:17:47 -0700

    Adjust horz sync

--- rtl/video_timing.sv
+++ rtl/video_timing.sv
@@ -18,45 +18,17 @@ module video_timing
     output reg  vbl 
 );
 
-// 224x256 timings
-
-//hbstart	256
-//height	224
-//htotal	382
-//refresh	59.949642
-//vbend	    16
-//vbstart	240
-//vtotal	262
-//width	    256
-
 // 6MHz
 wire [8:0] HBL_START  = 256;
-wire [8:0] HS_START   = 288;
-wire [8:0] HS_END     = 320;
+wire [8:0] HS_START   = 296;
+wire [8:0] HS_END     = 328;
 wire [8:0] HTOTAL     = 383;
-
-//wire [8:0] VBL_START  = 224;
-//wire [8:0] VS_START   = 225;
-//wire [8:0] VS_END     = 248;
-//wire [8:0] VTOTAL     = 256;
 
 wire [8:0] VBL_START  = 223;
 wire [8:0] VBL_END    = 263;
 wire [8:0] VS_START   = 240;
 wire [8:0] VS_END     = 248;
 wire [8:0] VTOTAL     = 263;
-
-
-// 4MHz
-//wire [8:0] HBL_START  = 256;
-//wire [8:0] HS_START   = 256;
-//wire [8:0] HS_END     = 258;
-//wire [8:0] HTOTAL     = 264;
-//
-//wire [8:0] VBL_START  = 224;
-//wire [8:0] VS_START   = 232;
-//wire [8:0] VS_END     = 236;
-//wire [8:0] VTOTAL     = 256;
 
 reg [8:0] v;
 reg [8:0] h;
